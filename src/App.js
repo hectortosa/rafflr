3f9ae3693ce9a0a0eea959cfd2b78812f1dc3213
Héctor Tortosa <hectortosa@icloud.com>
2021-11-22 00:12:58 +0100

    Create effect of raffle happening for increased excitement
    * Added effect (hint) of raffle happening
    * Re-styled

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import React, { Component } from 'react';
-import logo from './images/header.png';
 import { shuffle } from 'shufflr';
 import './App.css';
 
@@ -7,13 +6,10 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <div className="App-header row">
-        <div className="col">
-          <img src={logo} className="App-logo" alt="logo" />
+        <div className="header">
           <h1>Rafflr App</h1>
-          </div>
         </div>
-        <Lottery prizesName="Prizes" participantsName="Participants" />
+        <Lottery className="main-content" prizesName="Prizes" participantsName="Participants" />
       </div>
     );
   }
@@ -44,6 +40,8 @@ class Lottery extends Component {
 
     this.participants = [];
     this.prizes = [];
+
+    this.soundEffect = new Audio('./sounds/raffle-effect.wav');
   }
 
   render () {
@@ -52,11 +50,11 @@ class Lottery extends Component {
         <div className="row">
           <div className="col">
             <DynamicList value={this.state.newPrize} name={this.props.prizesName} listItems={this.prizeList}
-              onSubmit={this.addPrize} onChange={this.handlePizesChange} />
+              onAdd={this.addPrize} onItemChange={this.handlePizesChange} />
           </div>
           <div className="col">
             <DynamicList value={this.state.newParticipant} name={this.props.participantsName} listItems={this.participantList}
-              onSubmit={this.addParticipant} onChange={this.handleParticipantsChange} />
+              onAdd={this.addParticipant} onItemChange={this.handleParticipantsChange} />
           </div>
         </div>
         <div className={"row"}>
@@ -68,7 +66,7 @@ class Lottery extends Component {
         </div>
         <div className="row">
           <div className="col">
-            <button onClick={this.runLottery}>Start Raffle</button>
+            <button disabled={false} onClick={this.runLottery}>Start Raffle</button>
           </div>
         </div>
         <div className={"row"}>
@@ -84,16 +82,22 @@ class Lottery extends Component {
     shuffledParticipants = [],
     raffleResult = {};
 
-    participantsList = this.buildParticipantsList(this.participants, this.prizes.length, "To Share");
+    for (var i = 1; i<=20; i++) {
+      setTimeout((function() {
+        raffleResult = [];
 
-    shuffledPrizes = shuffle(this.prizes);
-    shuffledParticipants = shuffle(participantsList);
+        participantsList = this.buildParticipantsList(this.participants, this.prizes.length, "To Share");
 
-    for (var i = 0; i < this.prizes.length; i++) {
-      this.getOrAddProperty(raffleResult, shuffledParticipants[i], []).push(shuffledPrizes[i]);
+        shuffledPrizes = shuffle(this.prizes);
+        shuffledParticipants = shuffle(participantsList);
+
+        for (var i = 0; i < this.prizes.length; i++) {
+          this.getOrAddProperty(raffleResult, shuffledParticipants[i], []).push(shuffledPrizes[i]);
+        }
+        
+        this.setState({ results: raffleResult });
+      }).bind(this), 100*i);
     }
-
-    this.setState({ results: raffleResult });
   }
 
   buildParticipantsList(participants, numberOfPrizes, spareParticipant) {
@@ -135,8 +139,13 @@ class Lottery extends Component {
     var newEntry = this.state.newPrize;
 
     newEntry.split(";").forEach((entry) => {
-      this.prizeList.push(<div key={entry.trim()}>{entry.trim()}</div>);
-      this.prizes.push(entry.trim());
+      if(this.prizes.includes(entry.trim())) {
+        console.log("price already in the list");
+      }
+      else {
+        this.prizeList.push(<div key={entry.trim()}>{entry.trim()}</div>);
+        this.prizes.push(entry.trim());
+      }
     });
 
     this.setState({ newPrize: "" });
@@ -152,8 +161,13 @@ class Lottery extends Component {
     var newEntry = this.state.newParticipant;
 
     newEntry.split(";").forEach((entry) => {
-      this.participantList.push(<div key={entry.trim()}>{entry.trim()}</div>);
-      this.participants.push(entry.trim());
+      if (this.participants.includes(entry.trim())) {
+        console.log("participant already in the list");
+      }
+      else {
+        this.participantList.push(<div key={entry.trim()}>{entry.trim()}</div>);
+        this.participants.push(entry.trim());
+      }
     });
 
     this.setState({ newParticipant: "" });
@@ -202,6 +216,12 @@ class Winner extends Component {
 }
 
 class DynamicList extends Component {
+  constructor(props) {
+    super(props);
+
+    this.addOnEnter = this.addOnEnter.bind(this);
+  }
+
   render() {
     return (
       <div>
@@ -209,11 +229,16 @@ class DynamicList extends Component {
         <div>
           {this.props.listItems}
         </div>
-        <form onSubmit={this.props.onSubmit}>
-          <input type="text" value={this.props.value} onChange={this.props.onChange} />
-        </form>
+        <input type="text" value={this.props.value} onChange={this.props.onItemChange} onKeyPress={this.addOnEnter} />
+        <a href="" onClick={this.props.onAdd}>Add</a>
       </div>
     );
+  }
+
+  addOnEnter (event) {
+    if (event.key === 'Enter') {
+      this.props.onAdd(event);
+    }
   }
 }
 
